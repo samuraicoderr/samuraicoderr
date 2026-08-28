4d4e7c7e3b0150435395fce4e7f42357e67b333e
Williams Samuel <131383522+samuraicoderr@users.noreply.github.com>
2026-08-29 00:09:23 +0100

    Update Social Links

--- src/lib/me.ts
+++ src/lib/me.ts
@@ -1,12 +1,12 @@
 const Me = {
     name: "Williams Samuel",
     avatar: "/me.jpg",
-    twitter: "https://x.com/samuraicoderr",
+    twitter: "https://x.com/thesamuraicoder",
     linkedin: "https://www.linkedin.com/in/samuraicoderr/",
-    instagram: "https://www.instagram.com/samurai_coderr/",
+    instagram: "https://www.instagram.com/samuraicoderr/",
     medium: "https://medium.com/@samuraicoderr",
     email: "williamusanga23@gmail.com",
-    github: "https://github.com/KingWilliamsGPT",
+    github: "https://github.com/samuraicoderr",
 }
 
 export default Me;
